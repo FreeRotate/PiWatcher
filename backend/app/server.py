9e91537a602027c48001b77879fb8ce181e3f2ce
LauTrueyes <lautrueyes@163.com>
2024-02-29 16:18:25 +0800

    Add a time display function

--- backend/app/server.py
+++ backend/app/server.py
@@ -4,6 +4,8 @@
 # @Author: LauTrueyes
 # @Date  : 2024/2/27 15:19
 import cv2
+import time
+from datetime import datetime
 from flask import Flask, render_template, Response
 
 app = Flask(__name__)
@@ -26,6 +28,11 @@ def generate_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+def generate_time():
+    while True:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        yield f"data: {current_time}\n\n"
+        time.sleep(1)  # Update time every second
 
 @app.route('/')
 def index():
@@ -34,6 +41,11 @@ def index():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/time_feed')
+def time_feed():
+    return Response(generate_time(), content_type='text/event-stream')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
